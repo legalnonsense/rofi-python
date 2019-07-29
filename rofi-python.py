27916df2d5b44af52fd0c122fb848005fcfba038
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-29 12:58:35 -0400

    changed into a class -- not sure if a good idea!

--- rofi-python.py
+++ rofi-python.py
@@ -1,51 +1,77 @@
 #!/usr/bin/env python3
 import subprocess
 
-
-def _process_rofi_output(s):
-    ts=s.split('\n')
-    strings=[]
+class rofi_python:
+    """Use python to run Rofi and get output from it."""
+    def __init__(self):
+        pass 
     
-    for x in range(len(ts)):
-        for s in ts[x].split("''"):
-            strings.append(s)
-                   
+    def __process_rofi_output(self, s):
+        """Private method to process Rofi's output and return data in the form of (last_query, results)"""
+        ts=s.split('\n')
+        strings=[]
 
-    for x in range(len(strings)):
+        for x in range(len(ts)):
+            for s in ts[x].split("''"):
+                strings.append(s)
 
-        strings[x] = strings[x].strip()
+        for x in range(len(strings)):
+            strings[x] = strings[x].strip()
+            try: 
+                if strings[x][0] == "'":
+                    strings[x]=strings[x][1:]
+                if strings[x][-1] == "'":
+                    strings[x]=strings[x][:-1]
+            except: pass 
+        last_query = strings[0].strip()
+        results = []
+        for ss in strings[1:]:
+            if (ss.strip()==last_query) or (ss.strip()==''):
+                pass
+            else:
+                results.append(ss)
+        return(last_query, results)
 
-        try: 
-            if strings[x][0] == "'":
-                strings[x]=strings[x][1:]
-            if strings[x][-1] == "'":
-                strings[x]=strings[x][:-1]
-        except: pass 
+    def run(self, rofi_command, inputs):
+        """Run Rofi with rofi_command (a command line string)
+                     and inputs (a list of items to choose from)
+           Returns a list in the format (last_query, results)"""
+        p = subprocess.Popen(rofi_command.split(), stdout = subprocess.PIPE, stdin = subprocess.PIPE)
+        p.stdin.write("\n".join(inputs).encode('utf-8'))
+        s=p.communicate()[0].decode('utf-8')
+        return __process_rofi_output(s)
 
-    #strings[x] = "'" + strings[x] + "'"
+    def dump(self, rofi_command, inputs, rofi_search):
+        """Run Rofi with rofi_command (a string), inputs (a list of choices), and rofi_search (the search text)
+           Returns inputs, filtered using rofi_search"""
+        rofi_command += " -dump"
+        rofi_command += " -filter " + rofi_search_text
+        p = subprocess.Popen(rofi_command.split(), stdout = subprocess.PIPE, stdin = subprocess.PIPE)
+        p.stdin.write("\n".join(inputs).encode('utf-8'))
+        s=p.communicate()[0].decode('utf-8')
+        return __process_rofi_output(s)
+
+
+
+if __name__=="__main__":
+    rofi = rofi_python()
+
+    rofi_command = 'rofi -dmenu "$@" -no-sort -l 50 -i -p Copy -format Fq -width 2500 -multi-select'
+    rofi_list =  ['these', 'are', 'your', 'choices']
+    rofi_search_text = 'r'
+
+    p = subprocess.Popen('find /home/jeff/Dropbox -type d'.split(), stdout=subprocess.PIPE)
+    out = p.communicate()
     
-    last_query = strings[0].strip()
-    results = []
 
-    for ss in strings[1:]:
-        if (ss.strip()==last_query) or (ss.strip()==''):
-            pass
-        else:
-            results.append(ss)
-
-    return(last_query, results)
-
-
-def run_rofi(rofi_command, inputs):
-    p = subprocess.Popen(rofi_command.split(), stdout = subprocess.PIPE, stdin = subprocess.PIPE)
-    p.stdin.write("\n".join(inputs).encode('utf-8'))
-    s=p.communicate()[0].decode('utf-8')
-    return process_rofi_output(s)
+    #Print(rofi.run(rofi_command, rofi_list))
+    dump=rofi.dump(rofi_command, rofi_list, rofi_search_text)
+    print(rofi.run(rofi_command, dump[1]))
+    
 
 
 
 
-print(run_rofi('rofi -dmenu "$@" -no-sort -l 50 -i -p Copy -format Fq -width 2500 -multi-select', ['these', 'are', 'your', 'choices']))
 
 
 
